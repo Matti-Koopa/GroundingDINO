1e24517e0b618655a5c0ca6963924aaed90ae0f8
Matthias Porges <matthias.porges@wien.gv.at>
2023-05-09 10:31:44 +0200

    Remove yapf

--- groundingdino/util/slconfig.py
+++ groundingdino/util/slconfig.py
@@ -11,7 +11,6 @@ from argparse import Action
 from importlib import import_module
 
 from addict import Dict
-from yapf.yapflib.yapf_api import FormatCode
 
 BASE_KEY = "_base_"
 DELETE_KEY = "_delete_"
@@ -297,13 +296,6 @@ class SLConfig(object):
 
         cfg_dict = self._cfg_dict.to_dict()
         text = _format_dict(cfg_dict, outest_level=True)
-        # copied from setup.cfg
-        yapf_style = dict(
-            based_on_style="pep8",
-            blank_line_before_nested_class_or_def=True,
-            split_before_expression_after_opening_paren=True,
-        )
-        text, _ = FormatCode(text, style_config=yapf_style, verify=True)
 
         return text
 
